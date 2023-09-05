00bfcbec4bc696784a66e54e4dac4ed587dedd54
JFerrettUK <jferrettuk@gmail.com>
2023-09-05 10:32:39 +0100

    pizza image

---
+++ src/Pizza.jsx
@@ -0,0 +1,7 @@
+function Pizza() {
+  return (
+    <img src="https://media-cdn.tripadvisor.com/media/photo-s/17/59/ba/8c/img-20190430-195141-largejpg.jpg" />
+  );
+}
+
+export default Pizza;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
+import Pizza from "./Pizza.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Greeting />
+    <Pizza />
   </React.StrictMode>
 );
