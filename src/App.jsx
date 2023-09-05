3dc48e250082e14165947d55be2244d6523c598e
JFerrettUK <jferrettuk@gmail.com>
2023-09-05 10:24:21 +0100

    Vite and React included

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot;42.&quot;</h1>;
+}
+
+export default Greeting;
