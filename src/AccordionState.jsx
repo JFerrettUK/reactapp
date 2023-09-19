9ea2e273e59533a22a3a99e55491db384287c9cf
JFerrettUK <jferrettuk@gmail.com>
2023-09-19 13:36:30 +0100

    working with state exercises

--- src/AccordionState.jsx
+++ src/AccordionState.jsx
@@ -1,35 +1,41 @@
 import { useState } from "react";
+import { foods, filterItems } from "./data.js";
 
-function Panel({ title, children }) {
-  const [isActive, setIsActive] = useState(false);
+export default function FilterableList() {
+  const [query, setQuery] = useState("");
+  const results = filterItems(foods, query);
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
   return (
-    <section className="panel">
-      <h3>{title}</h3>
-      {isActive ? (
-        <p>{children}</p>
-      ) : (
-        <button onClick={() => setIsActive(true)}>Show</button>
-      )}
-    </section>
+    <>
+      <SearchBar query={query} onChange={handleChange} />
+      <hr />
+      <List items={results} />
+    </>
+  );
+}
+function SearchBar({ query, onChange }) {
+  return (
+    <label>
+      Search: <input value={query} onChange={onChange} />
+    </label>
   );
 }
 
-export default function Accordion() {
+function List({ items }) {
   return (
-    <>
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel title="About">
-        With a population of about 2 million, Almaty is Kazakhstan's largest
-        city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel title="Etymology">
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
-        "apple" and is often translated as "full of apples". In fact, the region
-        surrounding Almaty is thought to be the ancestral home of the apple, and
-        the wild
-        <i lang="la">Malus sieversii</i> is considered a likely candidate for
-        the ancestor of the modern domestic apple.
-      </Panel>
-    </>
+    <table>
+      <tbody>
+        {items.map((food) => (
+          <tr key={food.id}>
+            <td>{food.name}</td>
+            <td>{food.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
